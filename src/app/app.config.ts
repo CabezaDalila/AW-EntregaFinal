ab84ed5db373ce12f2ec6aafdb0902b977966c81
Franco Benettini <franbenettini@gmail.com>
2024-10-03 18:38:10 -0300

    Primer commit - Subiendo proyecto a GitHub

---
+++ src/app/app.config.ts
@@ -0,0 +1,18 @@
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { provideAuth0 } from '@auth0/auth0-angular';
+
+import { routes } from './app.routes';
+
+export const appConfig: ApplicationConfig = {
+  providers: [
+    provideRouter(routes),
+    provideAuth0({
+      domain: 'dev-qerco0ngptsqmvbv.us.auth0.com',
+      clientId: '3FktI24ALRH8ZskCYYmxB2GSp92Zmtfo',
+        authorizationParams: {
+          redirect_uri: window.location.origin
+        }
+      })
+  ]
+};
