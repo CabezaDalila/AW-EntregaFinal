d18222883da8c4489d7c28a6d5bd2d268af6bfac
Franco Benettini <franbenettini@gmail.com>
2024-10-03 22:40:42 -0300

    logo

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,15 @@
 import { Routes } from '@angular/router';
 import { WelcomeComponent } from './components/welcome/welcome.component';
 import { DashboardComponent } from './components/dashboard/dashboard.component';
+import { AuthGuard } from '@auth0/auth0-angular';
 
 export const routes: Routes = [
     { path: '', redirectTo: 'welcome', pathMatch: 'full' },
     { path: 'welcome', component: WelcomeComponent },
-    { path: 'dashboard', component: DashboardComponent },
-    { path: '**', redirectTo: 'welcome', pathMatch: 'full'}
+    { 
+        path: 'dashboard', 
+        component: DashboardComponent,
+        canActivate: [AuthGuard]
+    },
+    { path: '**', redirectTo: 'dashboard' }
 ];
--- src/app/components/dashboard/dashboard.component.ts
+++ src/app/components/dashboard/dashboard.component.ts
@@ -1,5 +1,6 @@
+import { Component, OnInit, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
@@ -9,14 +10,22 @@ import { AuthService } from '@auth0/auth0-angular';
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
-export class DashboardComponent {
+export class DashboardComponent implements OnInit {
+  constructor(
+    public auth: AuthService,
+    private router: Router
+  ) {}
 
-  constructor(public auth: AuthService) {
+  @HostListener('window:popstate', ['$event'])
+  onPopState(event: any) {
+    history.pushState(null, '', '/dashboard');
+  }
 
+  ngOnInit() {
+    history.pushState(null, '', '/dashboard');
   }
 
   logOut() {
-    this.auth.logout();
+    this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
   }
-
 }
