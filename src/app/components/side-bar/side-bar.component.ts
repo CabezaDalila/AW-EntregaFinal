46349ca1dcfcad4f7ba8b6441aa01e88b3c52c78
dalila <dalilagiselcabeza@gmail.com>
2024-10-26 22:28:05 -0300

    implemente que se pueda contraer y expandir el menu

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,27 +1,27 @@
 import { CommonModule } from '@angular/common';
 import { Component, OnInit } from '@angular/core';
+import { RouterModule } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
 
 @Component({
   selector: 'app-side-bar',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterModule],
   templateUrl: './side-bar.component.html',
   styleUrl: './side-bar.component.scss'
 })
 export class SideBarComponent implements OnInit {
   userName?: string = '';
   userPicture?: string = '';
-
+  isOpen = true;
   constructor(public auth: AuthService) {
   }
   ngOnInit(): void {
     this.auth.user$.subscribe((user) => { this.userName = user?.name; this.userPicture = user?.picture });
   }
-  isOpen = true;
-
   toggleSidebar() {
     this.isOpen = !this.isOpen;
   }
+
 }
 
