0f63c83015e83ec6347fbe1daaf2f7d4f1f64601
dalila <dalilagiselcabeza@gmail.com>
2024-11-20 19:28:52 -0300

    iguale al main

--- src/app/components/side-bar/side-bar.component.ts
+++ src/app/components/side-bar/side-bar.component.ts
@@ -1,28 +1,54 @@
 import { CommonModule } from '@angular/common';
-import { Component, OnInit } from '@angular/core';
-import { RouterModule } from '@angular/router';
+import { Component, HostListener, OnInit } from '@angular/core';
+import { Router, RouterModule } from '@angular/router';
 import { AuthService } from '@auth0/auth0-angular';
+
 @Component({
   selector: 'app-side-bar',
   standalone: true,
   imports: [CommonModule, RouterModule],
   templateUrl: './side-bar.component.html',
-  styleUrl: './side-bar.component.scss'
+  styles: []
 })
 export class SideBarComponent implements OnInit {
   userName?: string = '';
   userPicture?: string = '';
   isOpen = true;
-  constructor(public auth: AuthService) {}
+  isConfigMenuOpen = false;
+
+  constructor(public auth: AuthService, private router: Router) {}
+
   ngOnInit(): void {
-    this.auth.user$.subscribe((user) => { 
-      this.userName = user?.name; 
-      this.userPicture = user?.picture; 
+    this.auth.user$.subscribe((user) => {
+      this.userName = user?.name;
+      this.userPicture = user?.picture;
     });
   }
-   
+
   toggleSidebar() {
     this.isOpen = !this.isOpen;
+    if (!this.isOpen) {
+      this.isConfigMenuOpen = false;
+    }
+  }
+ 
+  toggleConfigMenu() {
+    this.isConfigMenuOpen = !this.isConfigMenuOpen;
+  }
+
+  @HostListener('document:click', ['$event'])
+  onDocumentClick(event: MouseEvent) {
+    if (this.isConfigMenuOpen) {
+      const configMenu = document.querySelector('.config-menu');
+      const configButton = document.querySelector('.config-button');
+      if (!configMenu?.contains(event.target as Node) && !configButton?.contains(event.target as Node)) {
+        this.isConfigMenuOpen = false;
+      }
+    }
+  }
+
+  logout() {
+    this.auth.logout({ logoutParams: { returnTo: window.location.origin } });
+    this.isConfigMenuOpen = false;
   }
 }
- 
