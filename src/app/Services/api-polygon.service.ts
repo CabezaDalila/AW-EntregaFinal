bda781ed793a6ab6813f8c788a9eeb1fbff46eef
AgustinAzul <agustds@gmail.com>
2024-11-21 17:22:55 -0300

    agregando cotizaciones principales

--- src/app/Services/api-polygon.service.ts
+++ src/app/Services/api-polygon.service.ts
@@ -4,14 +4,18 @@ import { Observable } from 'rxjs';
 import { IDailyStocksResponse } from '../interfaces/IdailyStocks';
 import { IStockDetailResponse } from '../interfaces/stock';
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class ApiPolygonService {
   private apiKey = '8nkipGR5ayMVs3Ax0tvUAuhhMxuWOa7z';
   private baseUrl = 'https://api.polygon.io/v2/aggs/ticker/';
-  constructor(private http: HttpClient) { }
+  constructor(private http: HttpClient) {}
 
-  getDetailStock(ticker: string, startDate: string, endDate: string): Observable<IStockDetailResponse> {
+  getDetailStock(
+    ticker: string,
+    startDate: string,
+    endDate: string
+  ): Observable<IStockDetailResponse> {
     const url = `${this.baseUrl}${ticker}/range/1/day/${startDate}/${endDate}?apiKey=${this.apiKey}`;
     return this.http.get<IStockDetailResponse>(url);
   }
@@ -19,5 +23,10 @@ export class ApiPolygonService {
   getDailyStocks(date: string): Observable<IDailyStocksResponse> {
     const url = `https://api.polygon.io/v2/aggs/grouped/locale/us/market/stocks/${date}?apiKey=${this.apiKey}`;
     return this.http.get<IDailyStocksResponse>(url);
+  }
+
+  getStockPreviousClose(ticker: string): Observable<any> {
+    const url = `${this.baseUrl}${ticker}/prev?apiKey=${this.apiKey}`;
+    return this.http.get(url);
   }
 }
